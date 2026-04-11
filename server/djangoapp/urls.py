d4bbac40a318ad1ddc83ede2ab7d8c72e2336374
Nathan-0001 <nathan.hartshorn24@gmail.com>
2026-04-11 14:43:48 +0100

    more changes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,7 +2,6 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic import TemplateView
 from . import views
 
 app_name = "djangoapp"
@@ -10,7 +9,6 @@ urlpatterns = [
     # # path for registration
     # path for login
     path(route="login", view=views.login_user, name="login"),
-    path("login/", TemplateView.as_view(template_name="index.html")),
     # path for dealer reviews view
     # path for add a review view
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
