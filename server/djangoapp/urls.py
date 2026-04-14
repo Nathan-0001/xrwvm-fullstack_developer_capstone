784eb5761f710ea5aab3668cfe82ea3e8e463e0b
Nathan-0001 <nathan.hartshorn24@gmail.com>
2026-04-14 22:12:09 +0100

    Adding car makes and models to django

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,8 +10,10 @@ urlpatterns = [
     path(route="register", view=views.registration, name="register"),
     # path for login
     path(route="login", view=views.login_user, name="login"),
-    #path for logout
+    # path for logout
     path(route="logout", view=views.logout_user, name="logout"),
     # path for dealer reviews view
     # path for add a review view
+    # path to add a view to get cars
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
